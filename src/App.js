0afd1ea9066aceba9e3317664166e8c8afc4c3e3
Timothy Shores <timothymshores@gmail.com>
2019-03-10 15:24:50 -0400

    Refactored app.js from class based component to  use React hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person'
 import './App.css';
 
-class App extends Component {
-    state = {
+const app = props => {
+    const [personState, setPersonState] = useState({
         persons: [
             { name: 'Tim', age: 30 },
             { name: 'Amanda', age: 26 }
         ]
-    }
+    });
 
-    switchNameHandler = () => {
-        // console.log('switchNameHandler')
-        this.setState({
+    const switchNameHandler = () => {
+        setPersonState({
             persons: [
                 { name: 'Timothy', age: 31 },
                 { name: 'Amanda Jayne', age: 27 }
             ]
-        })
+        }
+        )
     }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+            <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+        </div>
+    );
 }
 
-export default App;
+export default app;
