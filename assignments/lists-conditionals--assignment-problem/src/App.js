1cd3f49f14d5d8de946c3e74e0fa83ee64272371
Timothy Shores <timothymshores@gmail.com>
2019-03-28 17:26:49 -0500

    Refactor App.js. Move charList map out of return statement but still inside render function

--- assignments/lists-conditionals--assignment-problem/src/App.js
+++ assignments/lists-conditionals--assignment-problem/src/App.js
@@ -23,21 +23,24 @@ class App extends Component {
     };
 
     render() {
+        const { inputText } = this.state;
+        const charList = inputText.split('').map((char, index) => (
+            <CharComponent
+                key={index}
+                char={char}
+                click={() => this.handleDelete(index)}
+            />
+        ));
+
         return (
             <div className="App">
                 <ol>
                     <li>✅Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
                     <br />
                     <input type="text" onChange={this.inputHandler} value={this.state.inputText} />
-                    <p>{this.state.inputText.length} characters long</p>
+                    <p>{inputText.length} characters long</p>
                     <ValidationComponent textLength={this.state.inputText.length} />
-                    {this.state.inputText.split('').map((char, index) => (
-                        <CharComponent
-                            key={index}
-                            char={char}
-                            click={() => this.handleDelete(index)}
-                        />
-                    ))}
+                    {charList}
                     <li>✅ Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
                     <li>✅ Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
                     <li>✅ Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
