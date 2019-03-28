c844ab7f3aebb6fbd82d6771df17f1ce61a229cf
Timothy Shores <timothymshores@gmail.com>
2019-03-28 16:59:56 -0500

    Remove specific CharComponent from input field on click

--- assignments/lists-conditionals--assignment-problem/src/App.js
+++ assignments/lists-conditionals--assignment-problem/src/App.js
@@ -15,6 +15,13 @@ class App extends Component {
         this.setState({ inputText: event.target.value });
     }
 
+    handleDelete = index => {
+        const text = this.state.inputText.split('');
+        this.setState({
+            inputText: text.slice(0, index).concat(text.slice(index + 1, text.length)).join('')
+        })
+    };
+
     render() {
         return (
             <div className="App">
@@ -24,14 +31,18 @@ class App extends Component {
                     <input type="text" onChange={this.inputHandler} value={this.state.inputText} />
                     <p>{this.state.inputText.length} characters long</p>
                     <ValidationComponent textLength={this.state.inputText.length} />
-                    {this.state.inputText.split('').map(char => (
-                        <CharComponent char={char} />
+                    {this.state.inputText.split('').map((char, index) => (
+                        <CharComponent
+                            key={index}
+                            char={char}
+                            click={() => this.handleDelete(index)}
+                        />
                     ))}
                     <li>✅ Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
                     <li>✅ Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
                     <li>✅ Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
                     <li>✅ Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-                    <li>When you click a CharComponent, it should be removed from the entered text.</li>
+                    <li>✅ When you click a CharComponent, it should be removed from the entered text.</li>
                 </ol>
                 <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
             </div>
--- assignments/lists-conditionals--assignment-problem/src/components/CharComponent.js
+++ assignments/lists-conditionals--assignment-problem/src/components/CharComponent.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const CharComponent = props => {
-    console.log('CharComponent', props);
     return (
         <div
             className="CharComponent"
+            onClick={props.click}
             style={{
                 display: 'inline-block',
                 padding: '16px',
                 textAlign: 'center',
                 margin: '16px',
                 border: '1px solid black'
-            }}>
+            }}
+        >
             <p>{props.char}</p>
         </div>
     );
